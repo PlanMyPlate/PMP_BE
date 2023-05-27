2f9a5cd284d494a169aa0e236dab6c8687eb366e
Ashley <ashuhleyt@gmail.com>
2023-05-26 20:45:19 -0700

    i think models are fully set up

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,7 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do 
-  describe 'relationships' do 
+  describe 'associations' do 
     it { should have_many(:user_recipes)}
     it { should have_many(:recipes).through(:user_recipes)}
   end
